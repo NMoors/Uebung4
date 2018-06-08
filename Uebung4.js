fc79eb65226022f07d990a939d1cabe363420248
Nauka Moors <nauka.moors@informatik.hs-fulda.de>
2018-06-08 09:51:58 +0200

    Version 1.3

--- Uebung4.js
+++ Uebung4.js
@@ -1,8 +1,44 @@
 function onSubmit (){
+    console.log("2");
 
-var name = document.getElementById("name").value;
-console.log(name);
+if(testeHausnummer()&& testePostleitzahl()) document.getElementById("Err").innerHTML = "";
+gibLebensdauer();
 }
+
 window.onload = function(){
-    document.getElementById("submit").onclick = onSubmit;
+    document.getElementById("Submit").onclick = onSubmit;
+    console.log("1");
+}
+
+function testePostleitzahl(){
+    console.log("3");
+    var e=document.getElementById("Postleitzahl");
+    var regex= /^[0-9]{5}\b/g;
+    
+    var res = regex.test(e.value);
+    console.log(res);
+
+    if(res == false) document.getElementById("err").innerHTML = "Postleitzahl stimmt nicht";
+    else document.getElementById("Err").innerHTML = "";
+    return res;
+}
+
+function testeHausnummer(){
+    var e=document.getElementById("Hausnummer");
+    var regex= /^[0-9]{1,3}[a-zA-Z]{0,1}\b/g
+    var res = regex.test(e.value);
+    if(res == false) document.getElementById("err").innerHTML = "Hausnummer stimmt nicht";
+    else document.getElementById("Err").innerHTML = "";
+    return res;
+}
+
+function gibLebensdauer(){
+    var e = document.getElementById("Geburtstag");
+    var date = new Date(e.value);
+    var today = new Date();
+    var sec = (today.getTime() - date.getTime()) /1000;
+    var we = Math.floor(sec / (7*86400));
+    var day = Math.floor((sec - (we*86400*7)) / 86400);
+    var rest = Math.floor(sec - ((day*86400)+(we*86400*7)));
+    document.getElementById("Alter").innerHTML = "Wochen: " + we + " Tage: " + day + " Sekunden: " + rest ;
 }
